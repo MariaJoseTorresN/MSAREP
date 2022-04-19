9f0a6f704cca12c7d0626acbe9c9e9d2791e2229
Sebastian Ospina <juansebastian200103@gmail.com>
2022-04-19 11:09:38 -0400

    Reduccion de tablas y servicos, dejar lo basico

--- src/main/java/edu/escuelaing/arep/model/Publicacion.java
+++ src/main/java/edu/escuelaing/arep/model/Publicacion.java
@@ -1,15 +1,11 @@
 package edu.escuelaing.arep.model;
 
 import java.io.Serializable;
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -19,53 +15,48 @@ public class Publicacion implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @ManyToOne
-    @JoinColumn(name = "IdUsuario", nullable = false)
-    private Usuario idUsuario;
-    @ManyToOne
-    @JoinColumn(name = "IdHilo", nullable = false)
-    private Hilo idHilo;
+    @Column(name="Author")
+    private String author;
+    @Column(name="Titulo")
+    private String titulo;
     @Column(name = "Texto")
     private String texto;
-    @Column(name = "CreationDate", nullable = false)
-    private Date creationDate;
-
-    public Publicacion(Usuario idUsuario, Hilo idHilo,String texto){
-        this.idUsuario = idUsuario;
-        this.idHilo = idHilo;
-        this.texto = texto;
-        this.creationDate = new Date();
-    }
-
-    public Publicacion(Long id, Usuario idUsuario, Hilo idHilo, String texto){
+    
+    public Publicacion(Long id, String author, String titulo, String texto) {
         this.id = id;
-        this.idUsuario = idUsuario;
-        this.idHilo = idHilo;
+        this.author = author;
+        this.titulo = titulo;
         this.texto = texto;
-        this.creationDate = new Date();
     }
-
-    public Publicacion(){}
-
+    public Publicacion(String author, String titulo, String texto) {
+        this.author = author;
+        this.titulo = titulo;
+        this.texto = texto;
+    }
     public Long getId() {
         return id;
     }
-    public Date getCreationDate() {
-        return creationDate;
+    public void setId(Long id) {
+        this.id = id;
     }
-    public Usuario getIdUsuario() {
-        return idUsuario;
+    public String getAuthor() {
+        return author;
     }
-    public Hilo getIdHilo() {
-        return idHilo;
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
     public String getTexto() {
         return texto;
     }
-    public void setCreationDate(Date creationDate) {
-        this.creationDate = creationDate;
-    }
     public void setTexto(String texto) {
         this.texto = texto;
     }
+
+
 }
--- src/main/java/edu/escuelaing/arep/rest/PublicacionREST.java
+++ src/main/java/edu/escuelaing/arep/rest/PublicacionREST.java
@@ -1,9 +1,4 @@
 package edu.escuelaing.arep.rest;
-
-import java.net.URI;
-
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,13 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import edu.escuelaing.arep.model.Hilo;
 import edu.escuelaing.arep.model.Publicacion;
-import edu.escuelaing.arep.model.Usuario;
-import edu.escuelaing.arep.service.HiloService;
 import edu.escuelaing.arep.service.PublicacionService;
-import edu.escuelaing.arep.service.UsuarioService;
 
 @RestController
 @RequestMapping("/api/publicacion")
@@ -25,10 +15,6 @@ public class PublicacionREST {
     
     @Autowired
     private PublicacionService publicacionService;
-    @Autowired
-    private UsuarioService usuarioService;
-    @Autowired
-    private HiloService hiloService;
 
     @PostMapping("/save")
     private ResponseEntity<Boolean> save(@RequestBody Publicacion publicacion) {
@@ -38,26 +24,5 @@ public class PublicacionREST {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }        
-    }
-    @PostMapping("/create")
-    private ResponseEntity<Publicacion> save(@PathParam("idUsuario")Long idUsuario, @PathParam("idHilo")Long idHilo) {
-        try {
-            Usuario usuario = usuarioService.findById(idUsuario).get();
-            Hilo hilo = hiloService.findById(idHilo).get();
-            Publicacion publicacion = new Publicacion(usuario, hilo, "");
-            Publicacion post =  publicacionService.create(publicacion);
-            return ResponseEntity.ok(post);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-    }
-    @PostMapping()
-    private ResponseEntity<Hilo> hilos(@RequestBody Hilo hilo) {
-        Hilo temp = hiloService.create(hilo);
-        try {
-            return ResponseEntity.created(new URI("/api/hilo"+temp.getId())).body(temp);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
     }
 }
